240730b8119a4deda322423a1398eeeee31e718f
Jacob Cheatley <jacobcheatley@gmail.com>
2024-01-27 12:49:24 +1300

    Game loop up until tell jokes

--- ggj-2024/main.py
+++ ggj-2024/main.py
@@ -14,50 +14,123 @@ MAX_PLAYERS = 4
 class ConnectionManager:
     def __init__(self):
         self.server_websocket: WebSocket = None
-        self.clients: dict[str, WebSocket] = {}
+        self.client_websockets: dict[str, WebSocket] = {}
 
     async def connect_client(self, client_name: str, websocket: WebSocket):
-        if client_name in self.clients:
-            raise ValueError("Name already exists")
-        if len(self.clients) >= MAX_PLAYERS:
-            raise ValueError("Game full!")
-
+        self.client_websockets[client_name] = websocket
         await websocket.accept()
-        await websocket.send_json({"type": "message", "data": f"Welcome {client_name}"})
-        await websocket.send_json({"type": "name", "data": client_name})
-        await websocket.send_json({"type": "state", "data": "JOKE"})
-        self.clients[client_name] = websocket
-
-        await self.send_server({"type": "player_join", "data": client_name})
-        await self.send_server({"type": "players", "data": list(self.clients.keys())})
+        await websocket.send_json({"type": "debug", "data": "Connected"})
 
     async def disconnect_client(self, client_name: str):
-        self.clients.pop(client_name)
-
-        await self.send_server({"type": "player_leave", "data": client_name})
-        await self.send_server({"type": "players", "data": list(self.clients.keys())})
+        self.client_websockets.pop(client_name)
 
     async def connect_server(self, websocket: WebSocket):
-        await websocket.accept()
-        await websocket.send_json({"type": "state", "data": "WAIT"})
         self.server_websocket = websocket
+        await websocket.accept()
+        await websocket.send_json({"type": "debug", "data": "Connected"})
 
     def disconnect_server(self):
         pass  # IDK what to do here
 
-    async def send_server(self, data: Any):
-        await self.send(self.server_websocket, data)
+    async def send_client(self, client_name: str, data: Any):
+        await self._send(self.client_websockets[client_name], data)
 
-    async def send(self, websocket: WebSocket, data: Any):
+    async def send_server(self, data: Any):
+        await self._send(self.server_websocket, data)
+
+    async def _send(self, websocket: WebSocket, data: Any):
         await websocket.send_json(data)
 
     async def broadcast(self, data: Any):
-        for client_name, websocket in self.clients.items():
+        for client_name, websocket in self.client_websockets.items():
             await websocket.send_json(data)
 
+    def is_player_connected(self, client_name: str):
+        return client_name in self.client_websockets
 
-manager = ConnectionManager()
 
+class Player:
+    def __init__(self, name: str, color: str | None = None) -> None:
+        self.name = name
+        self.color = color
+
+
+class Game:
+    def __init__(self, connection: ConnectionManager) -> None:
+        self.connection = connection
+        self.server_state = "INIT"
+        self.players: dict[str, Player] = {}
+        self.player_jokes: dict[str, str] = {}
+
+    async def connect_server(self, websocket: WebSocket):
+        await self.connection.connect_server(websocket)
+        await websocket.send_json({"type": "state", "data": "WAIT_FOR_PLAYERS"})
+
+    async def send_player(self, name: str, data: Any):
+        await self.connection.send_client(name, data)
+
+    async def send_server(self, data: Any):
+        await self.connection.send_server(data)
+
+    async def player_join(self, client_name: str, websocket: WebSocket):
+        # if self.server_state != "WAITING_FOR_PLAYERS":
+        #     raise ValueError("Game already in progress!")
+        if len(self.players) >= MAX_PLAYERS:
+            raise ValueError("Game full!")
+        if client_name in self.players and self.connection.is_player_connected(client_name):
+            raise ValueError("Name already exists")
+
+        await self.connection.connect_client(client_name, websocket)
+        self.players[client_name] = Player(client_name, "red")
+
+        await self.connection.send_client(client_name, {"type": "message", "data": f"Welcome {client_name}"})
+        await self.connection.send_client(client_name, {"type": "name", "data": client_name})
+        await self.connection.send_client(client_name, {"type": "state", "data": "WAIT"})
+
+        await self.send_server({"type": "player_join", "data": client_name})
+        await self.send_server({"type": "players", "data": list(self.players.keys())})
+
+    async def player_leave(self, client_name: str):
+        await self.connection.disconnect_client(client_name)
+
+        await self.send_server({"type": "player_leave", "data": client_name})
+        await self.send_server({"type": "players", "data": list(self.players.keys())})
+
+    async def process_server(self, data: dict[str, Any]):
+        match data:
+            case {"type": "state_set", "data": state}:
+                self.server_state = state
+                match state:
+                    case "ASK_FOR_JOKE":
+                        await self.goto_ask_for_jokes()
+            case {"type": "start"}:
+                await self.goto_game_start()
+            case {"type": "skip"}:
+                await self.goto_tell_jokes()
+
+    async def goto_game_start(self):
+        await self.connection.send_server({"type": "state", "data": "START_GAME"})
+
+    async def goto_ask_for_jokes(self):
+        await self.connection.broadcast({"type": "state", "data": "JOKE"})
+
+    async def goto_tell_jokes(self):
+        await self.connection.send_server({"type": "set_jokes", "data": self.player_jokes})
+        await self.connection.send_server({"type": "state", "data": "TELL_JOKES"})
+
+    async def process_player(self, name: str, data: dict[str, Any]):
+        player = self.players[name]
+        match data:
+            case {"type": "joke", "data": joke_text}:
+                print(f"{name} told joke {joke_text}")
+                await self.connection.send_server({"type": "submitted_joke", "data": {"player": player.name}})
+                self.player_jokes[name] = joke_text
+                if len(self.player_jokes) == len(self.players):
+                    await self.goto_tell_jokes()
+
+
+connection = ConnectionManager()
+game = Game(connection)
 app = FastAPI()
 
 
@@ -78,19 +151,23 @@ app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 @app.websocket("/ws_server")
 async def server_websocket_endpoint(websocket: WebSocket):
-    await manager.connect_server(websocket)
+    await game.connect_server(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
+            data = await websocket.receive_json()
             print(f"Server sent {data}")
+            await game.process_server(data)
+
     except WebSocketDisconnect:
-        manager.disconnect_server(websocket)
+        connection.disconnect_server(websocket)
+        print("Server disconnected")
 
 
 @app.websocket("/ws/{client_name}")
 async def websocket_endpoint(websocket: WebSocket, client_name: str):
     try:
-        await manager.connect_client(client_name, websocket)
+        # await connection.connect_client(client_name, websocket)
+        await game.player_join(client_name, websocket)
     except ValueError as e:
         await websocket.accept()
         await websocket.send_json({"type": "error", "data": str(e)})
@@ -100,8 +177,10 @@ async def websocket_endpoint(websocket: WebSocket, client_name: str):
 
     try:
         while True:
-            data = await websocket.receive_text()
+            data = await websocket.receive_json()
             print(f"{client_name} sent {data}")
+            await game.process_player(client_name, data)
+
     except WebSocketDisconnect:
-        await manager.disconnect_client(client_name)
+        await game.player_leave(client_name)
         print(f"{client_name} disconnected")
