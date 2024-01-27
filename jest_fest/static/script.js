a02d0a7112b5609cb2c61b0ed508bcd8fbd58e2f
Jacob Cheatley <jacobcheatley@gmail.com>
2024-01-27 18:22:39 +1300

    Basic music autoplay

--- jest_fest/static/script.js
+++ jest_fest/static/script.js
@@ -34,3 +34,9 @@ class StateMachine {
         this.current_state?.submit?.();
     }
 }
+
+let random_int = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
